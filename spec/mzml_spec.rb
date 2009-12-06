2df5735da80d6db8dcdd599a0de20f1a99ef8f48
Angel Pizarro <angel@upenn.edu>
2009-12-06 15:58:04 +0000

    wrote a minimal set of specifications and all pass. w00t!

--- spec/mzml_spec.rb
+++ spec/mzml_spec.rb
@@ -1,20 +1,48 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe MzML do
-  before(:all) do 
+  before(:all) do
     # set the input file name
-    @file = "small.mzML"
-    @compressed = "small.compressed.mzML"
+    @file = File.join(File.dirname(__FILE__),  "small.mzML")
+    @compressed = File.join(File.dirname(__FILE__),  "small.compressed.mzML")
+    @mgf = File.join(File.dirname(__FILE__),  "small.mgf")
   end
-  
-  
-  context "Given an valid mzML file" do 
-    it "I should be able to open the mzML file"
-    it "should read the index"
-    it "should get scan=506"
-    it "should unmarshall the first 64 byte mz array"  
-    it "should unmarshall the first 32 byte intensity array"
-    it "should unmarshall and uncompress the 64 byte mz array"  
-    it "should unmarshall and uncompress the 32 byte intensity array"
+
+  context "Given a valid mzML file" do
+    it "I should be able to open the mzML file" do
+      file = MzML::Doc.new(@file)
+      file.should(be_a_kind_of(MzML::Doc))
+    end
+    it "should read the index" do
+      file = MzML::Doc.new(@file)
+      file.index.should_not be_nil
+    end
+    it "should get the first spectrum" do
+      file = MzML::Doc.new(@file)
+      file.index.should_not be_nil
+    end
+    it "should unmarshall the a 64 byte mz array" do
+      mz = MzML::Doc.new(@file)
+      s = mz.spectrum(mz.index[:spectrum].keys.first)
+      s.mz.should_not be_nil
+    end
+    it "should unmarshall the a 32 byte intensity array" do
+      mz = MzML::Doc.new(@file)
+      s = mz.spectrum(mz.index[:spectrum].keys.first)
+      s.intensity.should_not be_nil
+    end
+  end
+  context "Given a valid mzML file that uses compression" do
+    it "should unmarshall and uncompress the 64 byte mz array" do
+      mz = MzML::Doc.new(@compressed)
+      s = mz.spectrum(mz.index[:spectrum].keys.first)
+      s.mz.should_not be_nil
+    end
+
+    it "should unmarshall and uncompress the 32 byte intensity array" do
+      mz = MzML::Doc.new(@compressed)
+      s = mz.spectrum(mz.index[:spectrum].keys.first)
+      s.intensity.should_not be_nil
+    end
   end
 end
