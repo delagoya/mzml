211f88313532dfb4f9a703574ca7a0f6a645c736
Angel Pizarro <angel@turaro.com>
2012-06-26 17:00:42 -0400

    added mgf fixture, renamed mzML source file, started on unit test

---
+++ test/test_mzml-helper.rb
@@ -0,0 +1,15 @@
+require 'test/unit'
+require 'mzml'
+
+class TestMzMLHelper < Test::Unit::TestCase
+  def setup
+    # set the input file name
+    @file = File.join(File.dirname(__FILE__),  "sample.mzML")
+    @compressed = File.join(File.dirname(__FILE__),  "sample.compressed.mzML")
+    @mgf = File.join(File.dirname(__FILE__),  "sample.mgf")
+  end
+  def test_canary
+    pass
+  end
+
+end
